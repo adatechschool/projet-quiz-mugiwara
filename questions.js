9a618fd100b37d27c324e96663c83cf6a4f38b40
Munoz <152181111+Kvrmea@users.noreply.github.com>
2024-10-29 11:18:32 +0100

    question v1

---
+++ questions.js
@@ -0,0 +1,106 @@
+export const quiz_one_piece = {
+    questions : [
+        {
+            text : "Quel est le trésor que recherche Luffy dans *One Piece* ?",
+            options : [
+                "La Perle des Mers",
+                "L'Émeraude Sacrée",
+                "Le One Piece",
+                "La Carte des îles Oubliées",
+            ],
+            correct_answer : "Le One Piece"
+        },
+        {
+            text : "Quel est le rêve de Luffy ?",
+            options : [
+                "Explorer toutes les îles du monde",
+                "Devenir le Roi des Pirates",
+                "Devenir le Pirate le plus riche du monde",
+                "Révéler les mystères du siècles oublié",
+            ],
+            correct_answer : "Devenir le Roi des Pirates"
+        },
+        {
+            text : "Comment s'appelle le groupe que Luffy forme avec ses amis ?",
+            options : [
+                "L'Alliance des Sept Mers",
+                "Les Corsaires du Grand Line",
+                "L'équipage du Chapeau de Paille",
+                "Les Pirates de l'Aube Rouge",
+            ],
+            correct_answer : "L'équipage du Chapeau de Paille"
+        },
+        {
+            text : "Quelle est la mer principale où se déroule l’aventure de Luffy ?",
+            options : [
+                "La Grande Line",
+                "La Route des Titans",
+                "L'Océan Sans Fin",
+                "La Mer de l'Ouest",
+            ],
+            correct_answer : "La Grande Line"
+        },
+        {
+            text : "Qui est l'auteur de *One Piece* ?",
+            options : [
+                "Eiichiro Oda",
+                "Masashi Kishimoto",
+                "Akira Toriyama",
+                "Tite Kubo",
+            ],
+            correct_answer : "Eiichiro Oda"
+        },
+        {
+            text : "Quel est le surnom donné à Luffy ?",
+            options : [
+                "Luffy la Flamme Bleue",
+                "Luffy l'Œil de Faucon",
+                "Luffy au Chapeau de Paille",
+                "Luffy le Phénix",
+            ],
+            correct_answer : "Luffy au Chapeau de Paille"
+        },
+        {
+            text : "Que représente le symbole sur le drapeau des pirates de Luffy ?",
+            options : [
+                "Deux sabres croisés derrière une crâne",
+                "Une tête de lion entourée de flammes",
+                "Un crâne avec un chapeau de paille",
+                "Un crâne avec une cicatrice en forme de croix",
+            ],
+            correct_answer : "Un crâne avec un chapeau de paille"
+        },
+        {
+            text : "Pourquoi Luffy a-t-il mangé un fruit du démon ?",
+            options : [
+                "Pour devenir plus fort et protéger son village",
+                "Parce qu'il voulait rivaliser avec Shanks",
+                "Pour se venger des pirates qui ont attaqué qon île",
+                "Par accident quand il était jeune",
+            ],
+            correct_answer : "Par accident quand il était jeune"
+        },
+        {
+            text : "Comment s’appelle le navire de Luffy et son équipage ?",
+            options : [
+                "L'Aurore des Mers",
+                "Le Thousand Sunny",
+                "Le Vogue Merry",
+                "Le Lion d'Or",
+            ],
+            correct_answer : "Le Thousand Sunny"
+        },
+        {
+            text : "Quelle est la mission de l'équipage de Luffy dans *One Piece* ?",
+            options : [
+                "Trouver le One Piece et réaliser leurs rêves respectifs",
+                "Défendre les îles de Grand Line contre Marine",
+                "Rassembler tous les fruits du démon pour obtenir le pouvoir ultime",
+                "Dévenir l'équipage le plus riche et influant du Nouveau Monde",
+            ],
+            correct_answer : "Trouver le One Piece et réaliser leurs rêves respectifs"
+        },
+    ]
+}
+
+console.log(projet_quiz_mugiwara.questions)
---
+++ script.js
@@ -0,0 +1,2 @@
+// Import des questions
+import { quiz_one_piece } from './questions.js'; // Import des questions
