aa8eda3d7f5926fa3a238d5e1bd644ba5c80c6dd
Munoz <152181111+Kvrmea@users.noreply.github.com>
2024-10-29 11:40:07 +0100

    script

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // Import des questions
 import { quiz_one_piece } from './questions.js'; // Import des questions
+
+// Variables pour suivre le quiz
+let currentQuestionIndex = 0 // On commence par la première question
+
+// Variables pour suivre le score qui commence de 0
+let score = 0
+
+// Sélection des éléments HTML
+const questionText = document.getElementById("question-text")
+const optionContainer = document.getElementById("option-container")
+const nextButton = document.getElementById("next-button")
+const replayButton = document.getElementById("replay-button")
+const resultElement = document.getElementById("result")
